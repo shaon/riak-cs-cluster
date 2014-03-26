d35b9771a4da91f5f88de32177953a3c450d43a1
shaon <mdshaonimran@gmail.com>
2014-03-26 22:31:52 +0600

    update cluster join

--- roles/join_nodes.rb
+++ roles/join_nodes.rb
@@ -4,3 +4,18 @@ run_list(
   "recipe[riak-join-nodes]"
 )
 
+default_attributes(
+  "riak_cs" => {
+    "config" => {
+      "riak_cs" => {
+        "admin_key" => "__string_ACCESSKEY",
+        "admin_secret" => "__string_SECRETKEY",
+        "stanchion_ip" => "__string_STANCHIONIP",
+        "stanchion_port" => 8085
+      }
+    }
+  },
+  "riak_join" => {
+    "node-head" => "__string_STANCHIONIP"
+  }
+)
